66b6283caa66a31b456d189444881335b28e21c4
Unknown <aaguilar819@west-mec.org>
2018-10-24 16:03:03 -0700

    Progress on validations

--- MessageBoard.php
+++ MessageBoard.php
@@ -31,19 +31,17 @@
             switch ($_GET['action']) {
                     //performs an action based on what was given
                 case 'Delete First':
-                    array_shift($messageArray);
+                    $messageArray = array_shift($messageArray);
                     break;
                 case 'Delete Last':
-                    array_pop($messageArray);
+                    $messageArray = array_pop($messageArray);
                     break;
                 case 'Delete Message':
-                    array_splice($messageArray, $_GET['message'], 1); // (does the job properly, but is not often used)
-                    /* $index = $_GET['message'];
-                    unset($messageArray[$index]);
-                    array_values($messageArray); // (required to ensure that unset works) */
-                    echo "<pre>\n";
-                    print_r($messageArray);
-                    echo "</pre>\n";
+                    $messageArray = array_splice($messageArray, $_GET['message'], 1); // (does the job properly, but is not often used)
+                    break;
+                case 'Remove Duplicates':
+                    $messageArray = array_unique($messageArray);
+                    $messageArray = array_values($messageArray);
                     break;
             }
             if (count($messageArray) > 0) {
@@ -74,15 +72,30 @@
         $messageArray = file("messages.txt");
         echo "<table style=\"background-color: lightgray;\" border=\"1\" width=\"100%\">\n";
         $count = count($messageArray);
+        //the for loop below changes the indexed array to an associative one, using the subject as the name for each string.
         for ($i = 0; $i < $count; $i++) {
             $currMsg = explode("~", $messageArray[$i]);
+            $keyMessageArray[$currMsg[0]] = $currMsg[1] . "~" . $currMsg[2];
+        }
+        
+        $index = 1;
+        //the key function returns where the pointer is within the array.
+        $key = key($keyMessageArray);
+        
+        //this liips through $keyMessageArray to display each piece of information, position, and the option to delete, all within a table.
+        foreach ($keyMessageArray as $message) {
+            $currMsg = explode("~", $message);
             echo "<tr>\n";
-            echo "<td width=\"5%\" style=\"text-align: center; font-weight: bold;\">" . ($i + 1) . "</td>\n";
-            echo "<td width = \"85%\"><span style=\"font-weight: bold;\">Subject: </span>" . htmlentities($currMsg[0]) . "<br>\n";
-            echo "<span style=\"font-weight: bold;\">Name: </span>" . htmlentities($currMsg[1]) . "<br>\n";
-            echo "<span style=\"text-decoration: underline; font-weight: bold;\">Message: </span><br>\n" . htmlentities($currMsg[2]) . "</td>\n";
-            echo "<td width=\"10%\"><a href='MessageBoard.php?" . "action=Delete%20Message" . "&message=$i'>Delete This Message</a></td>\n";
+            echo "<td width=\"5%\" style=\"text-align: center; font-weight: bold;\">" . $index . "</td>\n";
+            echo "<td width = \"85%\"><span style=\"font-weight: bold;\">Subject: </span>" . htmlentities($key) . "<br>\n";
+            echo "<span style=\"font-weight: bold;\">Name: </span>" . htmlentities($currMsg[0]) . "<br>\n";
+            echo "<span style=\"text-decoration: underline; font-weight: bold;\">Message: </span><br>\n" . htmlentities($currMsg[1]) . "</td>\n";
+            echo "<td width=\"10%\"><a href='MessageBoard.php?" . "action=Delete%20Message" . "&message=" . ($index - 1) . "'>Delete This Message</a></td>\n";
             echo "</tr>\n";
+            $index++;
+            // the line bleow moves the pointer in $keyMessageArray and re-locates the pointer's position
+            next($keyMessageArray);
+            $key = key($keyMessageArray);
         }
         echo "</table>";
     }
@@ -91,6 +104,7 @@
         <a href="PostMessage.php">Post New Message</a><br>
         <a href="MessageBoard.php?action=Delete%20First">Delete First Message</a><br>
         <a href="MessageBoard.php?action=Delete%20Last">Delete Last Message</a><br>
+        <a href="MessageBoard.php?action=Remove%20Duplicates">Remove Duplicates</a><br>
     </p>
 </body>
 
